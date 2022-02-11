a636fad9392e442c2904c4c65efee0105d1c16db
htouqeer938 <htouqeer938@gmail.com>
2022-02-11 17:13:13 +0500

    Setting Cors Error Access Control Origin

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -11,6 +12,12 @@ const students = require('./api/students');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +31,13 @@ app.use(function (req, res, next) {
   err.status = 404;
   next(err);
 });
+
+// Allow All origin
+// app.use(cors({
+//   origin: '*'
+// }));
+
+
 
 // error handler
 app.use(function (err, req, res, next) {
