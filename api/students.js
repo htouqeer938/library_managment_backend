3fa6b748c5bad30efc8e0eff8698af2280816594
htouqeer938 <htouqeer938@gmail.com>
2022-02-12 01:25:24 +0500

    no message

--- api/students.js
+++ api/students.js
@@ -27,106 +27,154 @@ function validbook(book) {
 
 // Student Routes Start
 router.get('/student/', (req, res) => {
-  queries.getAllstudent().then(students => {
-    res.json(students);
-  });
+  try {
+    queries.getAllstudent().then(students => {
+      res.json(students);
+    });
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 router.get('/student/:id', isValidId, (req, res, next) => {
-  queries.getOnestudent(req.params.id).then(student => {
-    if (student) {
-      res.json(student);
-    } else {
-      next();
-    }
-  });
+  try {
+    queries.getOnestudent(req.params.id).then(student => {
+      if (student) {
+        res.json(student);
+      } else {
+        next();
+      }
+    });
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 router.post('/student/', (req, res, next) => {
-  if (validstudent(req.body)) {
-    queries.createstudent(req.body).then(students => {
-      res.json(students[0]);
-    });
-  } else {
-    next(new Error('Invalid student'));
+  try {
+    if (validstudent(req.body)) {
+      queries.createstudent(req.body).then(students => {
+        res.json(students[0]);
+      });
+    } else {
+      next(new Error('Invalid student'));
+    }
+  } catch (error) {
+    res.json(error);
   }
 });
 
 router.put('/student/:id', isValidId, (req, res, next) => {
-  if (validstudent(req.body)) {
-    queries.updatestudent(req.params.id, req.body).then(students => {
-      res.json(students[0]);
-    });
-  } else {
-    next(new Error('Invalid student'));
+  try {
+    if (validstudent(req.body)) {
+      queries.updatestudent(req.params.id, req.body).then(students => {
+        res.json(students[0]);
+      });
+    } else {
+      next(new Error('Invalid student'));
+    }
+  } catch (error) {
+    res.json(error);
   }
 });
 
 router.delete('/student/:id', isValidId, (req, res) => {
-  queries.deletestudent(req.params.id).then(() => {
-    res.json({
-      deleted: true
+  try {
+    queries.deletestudent(req.params.id).then(() => {
+      res.json({
+        deleted: true
+      });
     });
-  });
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 router.get('/studentcount', (req, res) => {
-  queries.countstudent().then((count_student) => {
-    res.json(count_student)
-  })
+  try {
+    queries.countstudent().then((count_student) => {
+      res.json(count_student)
+    })
+  } catch (error) {
+    res.json(error);
+  }
 });
 // Student Routes End
 
 // Books Routes Start
 
 router.get('/book/', (req, res) => {
-  queries.getAllbook().then(books => {
-    res.json(books);
-  });
+  try {
+    queries.getAllbook().then(books => {
+      res.json(books);
+    });
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 router.get('/book/:id', isValidId, (req, res, next) => {
-  queries.getOnebook(req.params.id).then(book => {
-    if (book) {
-      res.json(book);
-    } else {
-      next();
-    }
-  });
+  try {
+    queries.getOnebook(req.params.id).then(book => {
+      if (book) {
+        res.json(book);
+      } else {
+        next();
+      }
+    });
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 router.post('/book/', (req, res, next) => {
-  if (validbook(req.body)) {
-    queries.createbook(req.body).then(book => {
-      res.json(book[0]);
-    });
-  } else {
-    next(new Error('Invalid student'));
+  try {
+    if (validbook(req.body)) {
+      queries.createbook(req.body).then(book => {
+        res.json(book[0]);
+      });
+    } else {
+      next(new Error('Invalid student'));
+    }
+  } catch (error) {
+    res.json(error);
   }
 });
 
 router.put('/book/:id', isValidId, (req, res, next) => {
-  if (validbook(req.body)) {
-    queries.updatebook(req.params.id, req.body).then(book => {
-      res.json(book[0]);
-    });
-  } else {
-    next(new Error('Invalid book'));
+  try {
+    if (validbook(req.body)) {
+      queries.updatebook(req.params.id, req.body).then(book => {
+        res.json(book[0]);
+      });
+    } else {
+      next(new Error('Invalid book'));
+    }
+  } catch (error) {
+    res.json(error);
   }
 });
 
 router.delete('/book/:id', isValidId, (req, res) => {
-  queries.deletebook(req.params.id).then(() => {
-    res.json({
-      deleted: true
+  try {
+    queries.deletebook(req.params.id).then(() => {
+      res.json({
+        deleted: true
+      });
     });
-  });
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 router.get('/bookcount', (req, res) => {
-  queries.countbook().then(count => {
-    res.json(count)
-  })
+  try {
+    queries.countbook().then(count => {
+      res.json(count)
+    })
+  } catch (error) {
+    res.json(error);
+  }
 });
 // Student Routes End
 
